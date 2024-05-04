1e3dc508f467f6ff2f7350da43612358be1670eb
frompaje <yanedwards11@gmail.com>
2024-05-03 23:07:19 -0300

    install cors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import fastify, { FastifyInstance } from "fastify";
 import { routerUserController } from "./controllers/user/router-user-controller";
 import { routerTaskController } from "./controllers/task/router-task.controller";
+import cors from "@fastify/cors";
 
 export const app: FastifyInstance = fastify();
+app.register(cors);
 
 app.register(routerUserController);
 app.register(routerTaskController);
